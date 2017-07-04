bbbb0ad10a39bc3094dfa8e4176d9c0a3ee02901
Jun Quan Tham <junquantham@Juns-iMac.local>
2017-07-04 15:19:41 +0800

    data extract in progress

---
+++ code/00_collection/003_get_user_photos.py
@@ -0,0 +1,77 @@
+import requests
+import json
+import configparser
+import sqlite3
+import time
+
+
+def write_to_config(config_path):
+    with open(config_path, 'w') as configfile:
+        v = int(progress) + counter
+        config.set('users', 'progress', str(v))
+        config.write(configfile)
+
+
+# Set the file paths
+config_path = './code/00_collection/config.ini'
+photo_json_dir = './data/json_user_photos/'
+db_path = './data/foursquare.db'
+
+# Read in the config.ini to get client_id and client_secret
+config = configparser.ConfigParser()
+config.read(config_path)
+
+app_to_use = 'app06'
+client_id = config[app_to_use]['client_id']
+client_secret = config[app_to_use]['client_secret']
+access_token = config[app_to_use]['access_token']
+
+# track progress
+progress = config['users']['progress']
+limit = config['users']['limit']
+
+# Set the param for the call
+param = {
+    'client_id': client_id,
+    'client_secret': client_secret,
+    'oauth_token': access_token,
+    'sort': 'recent',
+    'limit': '200',
+    'v': '20170625'
+}
+
+# connect and write to database
+conn = sqlite3.connect(db_path)
+c = conn.cursor()
+
+c.execute('SELECT DISTINCT uid FROM users LIMIT ' + limit + ' OFFSET ' + progress + ';')
+uids = c.fetchall()
+
+counter = 0
+
+for uid in uids:
+
+    uid = uid[0]
+    photo_record = []
+
+    param_str = '&'.join(['='.join(i) for i in param.items()])
+
+    # make the API Call
+    response = requests.get('https://api.foursquare.com/v2/users/' + str(uid) + '/photos?' + param_str)
+    parsed = response.json()
+
+    print(uid, parsed['meta'])
+
+    # write the raw response to file
+    outfile = open(photo_json_dir + str(uid) + '.json', 'w')
+    json.dump(parsed, outfile)
+    outfile.close()
+
+    counter += 1
+
+    if counter % 100 == 0:
+        write_to_config(config_path)
+        time.sleep(5)
+        print('Processed user #', counter)
+
+write_to_config(config_path)
---
+++ code/01_ingest/011_ingest_tips_users.py
@@ -0,0 +1,82 @@
+import os
+import json
+import sqlite3
+
+tips_json_dir = './data/json_tips/'
+
+# connect database
+conn = sqlite3.connect('./data/foursquare.db')
+c = conn.cursor()
+c.execute('DELETE FROM tips;')
+c.execute('DELETE FROM users;')
+conn.commit()
+
+
+uids = []
+cids = []
+
+counter = 0
+
+for filename in os.listdir(tips_json_dir):
+
+    json_file = open(tips_json_dir + filename)
+    raw_json = json_file.readlines()[0]
+
+    parsed = json.loads(raw_json,encoding='utf-8')
+
+    comment_records = []
+    user_records = []
+
+    rid = filename
+    try:
+        x = parsed['response']['tips']
+    except:
+        print(filename, ' | ',parsed)
+        continue
+
+    print(parsed)
+
+    for i in parsed['response']['tips']['items']:
+
+        uid = i['user']['id']
+        gender = i['user']['gender']
+
+        cid = i['id']
+        text = i['text']
+
+        try:
+            firstName = i['user']['firstName']
+        except:
+            firstName = ''
+        try:
+            lastName = i['user']['lastName']
+        except:
+            lastName = ''
+
+        if uid not in uids:
+            user_records.append((uid, firstName, lastName, gender))
+            uids.append(uid)
+
+        if cid not in cids:
+            comment_records.append((rid, uid, text))
+            cids.append(cid)
+
+    # prepare and execute statement
+    for record in user_records:
+        var_str = ', '.join('?' * len(record))
+        query_str = 'INSERT INTO users VALUES (%s);' % var_str
+        c.execute(query_str, record)
+
+    for record in comment_records:
+        var_str = ', '.join('?' * len(record))
+        query_str = 'INSERT INTO tips VALUES (%s);' % var_str
+        c.execute(query_str, record)
+
+    conn.commit()
+
+    counter += 1
+
+    if counter % 100 == 0:
+        print("Completed processing file # ",counter)
+
+conn.close()
