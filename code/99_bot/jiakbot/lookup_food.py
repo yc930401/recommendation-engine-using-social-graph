26d4e2fe5e21474e1b023182c53151ba20f76405
April <740402688@qq.com>
2017-07-19 19:19:40 +0800

    chatbot

--- code/99_bot/jiakbot/lookup_food.py
+++ code/99_bot/jiakbot/lookup_food.py
@@ -9,13 +9,8 @@ conn = sqlite3.connect(db_path)
 c = conn.cursor()
 c.execute('SELECT rid, tip FROM tips;')
 results = c.fetchall()
-conn.close()
 
-# food_list from wordnet
-#food = wn.synset('food.n.02')
-#food_list = list(set([w.replace('_', ' ') for s in food.closure(lambda s:s.hyponyms()) for w in s.lemma_names()]))
 food_list = []
-
 # food_list from existing file
 f = open('D:/Workspace-Github/saproject/code/99_bot/jiakbot/corpus/knowledge/foods.txt', 'r')
 for i in range(53):
@@ -27,12 +22,19 @@ f = open('D:/Workspace-Github/saproject/code/99_bot/jiakbot/corpus/knowledge/foo
 for i in range(120):
     food_list.append(f.readline().split('\n')[0].lower())
 f.close()
-
 print(food_list)
+
+records = []
 food_venue = open('D:/Workspace-Github/saproject/code/99_bot/jiakbot/corpus/knowledge/food_venue.txt', 'w', encoding = 'utf-8')
 for rid, tip in results:
     for food in food_list:
         if food in tip.lower():
             food_venue.write('%s\t%s\n' % (food, rid))
+            records.append(str((rid, food)))
 food_venue.close()
 
+query_str = 'INSERT INTO venues_food VALUES ' + ', '.join(records) + ';'
+print(query_str)
+c.execute(query_str)
+conn.commit()
+conn.close()
