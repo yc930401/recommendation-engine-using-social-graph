9d0c15a41146758f5f5729a6b66f733fa3e970a5
hanyingong <hanying.ong@outlook.com>
2017-07-26 02:26:39 +0800

    admin cleanup for web

--- docs/js/05_recommendation_table.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById('myDIV');
-    if (x.style.display != 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
-}
